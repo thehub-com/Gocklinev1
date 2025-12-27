faa6240434fa566ed1cdfadc757f904d74a02c69
thehub-com <korada209@gmail.com>
2025-12-27 17:20:06 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  BOT_TOKEN: "8261801832:AAEHUDbVv1lnBCjHtao_oeGNT_ODowA6Q8g",
+
+  ADMINS: [
+    5516708022 // ← ТВОЙ telegram ID
+  ],
+
+  MODERATORS: [
+    // сюда потом можно добавлять
+  ]
+}
